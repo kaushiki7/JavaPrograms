a67888403f2a34258fecd9209a84cb09da429b39
Kaushiki <161210025@nitdelhi.ac.in>
2019-09-19 13:45:54 +0530

    Update Sort.java

--- FirstLab22Aug/Sort.java
+++ FirstLab22Aug/Sort.java
@@ -1,4 +1,4 @@
-/*1. Write a Java program to sort a numeric array and a string array.package firstLab*/
+/*1. Write a Java program to sort a numeric array and a string array.*/
 
 import java.util.Scanner;
 import java.util.Arrays; 
